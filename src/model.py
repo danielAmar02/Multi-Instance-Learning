d8852d4248f747389d8130d7090b4424e488f63f
Daniel Amar <58550965+danielAmar02@users.noreply.github.com>
2022-05-29 14:25:46 +0300

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,304 @@
+import sys
+import time
+from random import shuffle
+import numpy as np
+import argparse
+import tensorflow as tf
+
+from keras.models import Model
+from keras.optimizers import SGD,Adam
+from keras.regularizers import l2
+from keras.layers import Input, Dense, Layer, Dropout, Conv2D, MaxPooling2D, Flatten, multiply
+
+
+
+from keras.layers import Layer
+from keras import backend as K
+from keras import activations, initializers, regularizers
+
+class Mil_Attention(Layer):
+    """
+    Mil Attention Mechanism
+    This layer contains Mil Attention Mechanism
+    # Input Shape
+        2D tensor with shape: (batch_size, input_dim)
+    # Output Shape
+        2D tensor with shape: (1, units)
+    """
+
+    def __init__(self, L_dim, output_dim, kernel_initializer='glorot_uniform', kernel_regularizer=None,
+                    use_bias=True, use_gated=True, **kwargs):
+        self.L_dim = L_dim
+        self.output_dim = output_dim
+        self.use_bias = use_bias
+        self.use_gated = use_gated
+
+        self.v_init = initializers.get(kernel_initializer)
+        self.w_init = initializers.get(kernel_initializer)
+        self.u_init = initializers.get(kernel_initializer)
+
+
+        self.v_regularizer = regularizers.get(kernel_regularizer)
+        self.w_regularizer = regularizers.get(kernel_regularizer)
+        self.u_regularizer = regularizers.get(kernel_regularizer)
+
+        super(Mil_Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+
+        assert len(input_shape) == 2
+        input_dim = input_shape[1]
+
+        self.V = self.add_weight(shape=(input_dim, self.L_dim),
+                                      initializer=self.v_init,
+                                      name='v',
+                                      regularizer=self.v_regularizer,
+                                      trainable=True)
+
+
+        self.w = self.add_weight(shape=(self.L_dim, 1),
+                                    initializer=self.w_init,
+                                    name='w',
+                                    regularizer=self.w_regularizer,
+                                    trainable=True)
+
+
+        if self.use_gated:
+            self.U = self.add_weight(shape=(input_dim, self.L_dim),
+                                     initializer=self.u_init,
+                                     name='U',
+                                     regularizer=self.u_regularizer,
+                                     trainable=True)
+        else:
+            self.U = None
+
+        self.input_built = True
+
+
+    def call(self, x, mask=None):
+        n, d = x.shape
+        ori_x = x
+        x = K.tanh(K.dot(x, self.V)) 
+
+        if self.use_gated:
+            gate_x = K.sigmoid(K.dot(ori_x, self.U))
+            ac_x = x * gate_x
+        else:
+            ac_x = x
+
+        soft_x = K.dot(ac_x, self.w)  
+        alpha = K.softmax(K.transpose(soft_x)) 
+        alpha = K.transpose(alpha)
+        return alpha
+
+    def compute_output_shape(self, input_shape):
+        shape = list(input_shape)
+        assert len(shape) == 2
+        shape[1] = self.output_dim
+        return tuple(shape)
+
+    def get_config(self):
+        config = {
+            'output_dim': self.output_dim,
+            'v_initializer': initializers.serialize(self.V.initializer),
+            'w_initializer': initializers.serialize(self.w.initializer),
+            'v_regularizer': regularizers.serialize(self.v_regularizer),
+            'w_regularizer': regularizers.serialize(self.w_regularizer),
+            'use_bias': self.use_bias
+        }
+        base_config = super(Mil_Attention, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class Last_Sigmoid(Layer):
+    """
+    Attention Activation
+    This layer contains a FC layer which only has one neural with sigmoid activation
+    and MIL pooling. The input of this layer is instance features. Then we obtain
+    instance scores via this FC layer. And use MIL pooling to aggregate instance scores
+    into bag score that is the output of Score pooling layer.
+    This layer is used in mi-Net.
+    # Arguments
+        output_dim: Positive integer, dimensionality of the output space
+        kernel_initializer: Initializer of the `kernel` weights matrix
+        bias_initializer: Initializer of the `bias` weights
+        kernel_regularizer: Regularizer function applied to the `kernel` weights matrix
+        bias_regularizer: Regularizer function applied to the `bias` weights
+        use_bias: Boolean, whether use bias or not
+        pooling_mode: A string,
+                      the mode of MIL pooling method, like 'max' (max pooling),
+                      'ave' (average pooling), 'lse' (log-sum-exp pooling)
+    # Input shape
+        2D tensor with shape: (batch_size, input_dim)
+    # Output shape
+        2D tensor with shape: (1, units)
+    """
+    def __init__(self, output_dim, kernel_initializer='glorot_uniform', bias_initializer='zeros',
+                    kernel_regularizer=None, bias_regularizer=None,
+                    use_bias=True, **kwargs):
+        self.output_dim = output_dim
+
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+
+        self.use_bias = use_bias
+        super(Last_Sigmoid, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 2
+        input_dim = input_shape[1]
+
+        self.kernel = self.add_weight(shape=(input_dim, self.output_dim),
+                                        initializer=self.kernel_initializer,
+                                        name='kernel',
+                                        regularizer=self.kernel_regularizer)
+
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.output_dim,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer)
+        else:
+            self.bias = None
+
+        self.input_built = True
+
+    def call(self, x, mask=None):
+        n, d = x.shape
+        x = K.sum(x, axis=0, keepdims=True)
+        # compute instance-level score
+        x = K.dot(x, self.kernel)
+        if self.use_bias:
+            x = K.bias_add(x, self.bias)
+
+        # sigmoid
+        out = K.sigmoid(x)
+
+
+        return out
+
+    def compute_output_shape(self, input_shape):
+        shape = list(input_shape)
+        assert len(shape) == 2
+        shape[1] = self.output_dim
+        return tuple(shape)
+
+    def get_config(self):
+        config = {
+            'output_dim': self.output_dim,
+            'kernel_initializer': initializers.serialize(self.kernel.initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+            'use_bias': self.use_bias
+        }
+        base_config = super(Last_Sigmoid, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+
+def bag_accuracy(y_true, y_pred):
+    """Compute accuracy of one bag.
+    Parameters
+    ---------------------
+    y_true : Tensor (N x 1)
+        GroundTruth of bag.
+    y_pred : Tensor (1 X 1)
+        Prediction score of bag.
+    Return
+    ---------------------
+    acc : Tensor (1 x 1)
+        Accuracy of bag label prediction.
+    """
+    y_true = K.mean(y_true, axis=0, keepdims=False)
+    y_pred = K.mean(y_pred, axis=0, keepdims=False)
+    acc = K.mean(K.equal(y_true, K.round(y_pred)))
+    return acc
+
+def recall_m(y_true, y_pred):
+    y_true = K.mean(y_true, axis=0, keepdims=False)
+    y_pred = K.mean(y_pred, axis=0, keepdims=False)
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    y_true = K.mean(y_true, axis=0, keepdims=False)
+    y_pred = K.mean(y_pred, axis=0, keepdims=False)
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def specificity_m(y_true, y_pred):
+    y_true = K.mean(y_true, axis=0, keepdims=False)
+    y_pred = K.mean(y_pred, axis=0, keepdims=False)
+    true_negatives = K.sum(K.round(K.clip(y_true * y_pred, 1, 0)))
+    predicted_negatives = K.sum(K.round(K.clip(y_pred, 1, 0)))
+    specificity = true_negatives / (predicted_negatives + K.epsilon())
+    return specificity
+
+def f1_m(y_true, y_pred):
+    y_true = K.mean(y_true, axis=0, keepdims=False)
+    y_pred = K.mean(y_pred, axis=0, keepdims=False)
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+def BA(y_true, y_pred):
+    y_true = K.mean(y_true, axis=0, keepdims=False)
+    y_pred = K.mean(y_pred, axis=0, keepdims=False)
+    balanced_acc = 0.5 * (specificity_m(y_true, y_pred) + recall_m(y_true, y_pred))
+    return balanced_acc 
+
+
+def bag_loss(y_true, y_pred):
+    """
+    Parameters
+    ---------------------
+    y_true : Tensor (N x 1)
+        GroundTruth of bag.
+    y_pred : Tensor (1 X 1)
+        Prediction score of bag.
+    """
+    y_true = K.mean(y_true, axis=0, keepdims=False)
+    y_pred = K.mean(y_pred, axis=0, keepdims=False)
+    loss = K.mean(-(y_true * K.log(y_pred) *(5/7) + (5/3) * (1 - y_true) * K.log(1 - y_pred)), axis=-1)
+    return loss 
+
+def cell_net(input_dim, lr=0.0001,weight_decay=0.0005, momentum=0.9,useMulGpu=False ):
+
+
+    data_input = Input(shape=input_dim, dtype='float32', name='input')
+    conv1 = Conv2D(36, kernel_size=(4,4), kernel_regularizer=l2(weight_decay), activation='relu')(data_input)
+    conv1 = MaxPooling2D((2,2))(conv1)
+
+    conv2 = Conv2D(48, kernel_size=(3,3),  kernel_regularizer=l2(weight_decay), activation='relu')(conv1)
+    conv2 = MaxPooling2D((2,2))(conv2)
+    x = Flatten()(conv2)
+
+    fc1 = Dense(512, activation='relu',kernel_regularizer=l2(weight_decay), name='fc1')(x)
+    fc1 = Dropout(0.5)(fc1)
+    fc2 = Dense(512, activation='relu', kernel_regularizer=l2(weight_decay), name='fc2')(fc1)
+    fc2 = Dropout(0.5)(fc2)
+
+
+    alpha = Mil_Attention(L_dim=128, output_dim=1, kernel_regularizer=l2(weight_decay), name='alpha', use_gated=False)(fc2)
+    x_mul = multiply([alpha, fc2])
+
+    out = Last_Sigmoid(output_dim=1, name='FC1_sigmoid')(x_mul)
+    
+    model = Model(inputs=[data_input], outputs=[out])
+    # model.summary()
+
+    if useMulGpu == True:
+        parallel_model = multi_gpu_model(model, gpus=2)
+        parallel_model.compile(optimizer=Adam(lr=lr, beta_1=0.9, beta_2=0.999), loss=bag_loss, metrics=[BA, precision_m])
+    else:
+        model.compile(optimizer=Adam(lr=lr, beta_1=0.9, beta_2=0.999), loss=bag_loss, metrics=[BA, precision_m])
+        parallel_model = model
+
+    return parallel_model
